c3136c84c717be23e23d374dff3fbbba96c74611
einav18-meet <einavcohen48@gmail.com>
2017-01-25 18:25:03 +0200

    Add files via upload

---
+++ einav-square.py
@@ -0,0 +1,11 @@
+from rectangle import Rectangle
+
+class Square(Rectangle):
+    
+
+    
+    def set_length(self,t_length):
+        super(Square,self).set_lenght(t_length,t_length)
+
+    
+        
